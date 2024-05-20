bdcfc85899045393d0fc55edb6256181b90f1730
alphantulukcu <alphantulukcu@gmail.com>
2024-05-20 12:43:11 +0300

    bug fixes

--- app/app.py
+++ app/app.py
@@ -350,7 +350,7 @@ def profile(user_id):
                     JOIN category c ON s.category_id = c.category_id
                     LEFT JOIN images i ON p.product_id = i.product_id
                     JOIN favorites f ON p.product_id = f.product_id
-                    WHERE f.cust_id = %s
+                    WHERE f.cust_id = %s and p.status = 1
                     GROUP BY p.product_id, b.user_id, s.subcategory_id, c.category_id
                 ''', (customer_id,))
                 favorites = cursor.fetchall()
